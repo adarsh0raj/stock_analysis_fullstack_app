28727acda1f53976b6287c65000a3b336f99af17
Adarsh <rajadarsh2602@gmail.com>
2022-10-21 07:01:13 +0530

    minor

--- frontend_temp/src/app/views/dashboard/dashboard.component.ts
+++ frontend_temp/src/app/views/dashboard/dashboard.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
-
-import { DashboardChartsData, IChartProps } from './dashboard-charts-data';
-
 
 @Component({
   templateUrl: 'dashboard.component.html',
